df71b97c89bc41de9f88ef7327be2ae1beb8357d
Mate Magyari <Mate.Magyari@Gamesys.co.uk>
2013-03-25 17:15:25 +0000

    add equals

--- src/main/java/home/gym/domain/model/event/EventNotificationType.java
+++ src/main/java/home/gym/domain/model/event/EventNotificationType.java
@@ -1,5 +1,8 @@
 package home.gym.domain.model.event;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class EventNotificationType {
 	
 	private String id;
@@ -8,30 +11,19 @@ public class EventNotificationType {
 		this.id = id;
 	}
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof EventNotificationType)) {
+            return false;
+        }
+        EventNotificationType castOther = (EventNotificationType) other;
+        return new EqualsBuilder().append(id, castOther.id).isEquals();
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EventNotificationType other = (EventNotificationType) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(id).append(id).hashCode();
+    }
 	
 	
 	
--- src/main/java/home/gym/domain/service/sender/specific/email/EmailMessage.java
+++ src/main/java/home/gym/domain/service/sender/specific/email/EmailMessage.java
@@ -1,65 +1,43 @@
 package home.gym.domain.service.sender.specific.email;
 
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+
 public class EmailMessage {
 
-	private String subject;
-	private String body;
-	private String to;
+    private String subject;
+    private String body;
+    private String to;
 
-	public void setTo(String to) {
-		this.to = to;
-	}
+    public void setTo(String to) {
+        this.to = to;
+    }
 
-	public void setBody(String body) {
-		this.body = body;
-		
-	}
+    public void setBody(String body) {
+        this.body = body;
+    }
 
-	public void setSubject(String subject) {
-		this.subject = subject;
-	}
-	
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((body == null) ? 0 : body.hashCode());
-		result = prime * result + ((subject == null) ? 0 : subject.hashCode());
-		result = prime * result + ((to == null) ? 0 : to.hashCode());
-		return result;
-	}
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		EmailMessage other = (EmailMessage) obj;
-		if (body == null) {
-			if (other.body != null)
-				return false;
-		} else if (!body.equals(other.body))
-			return false;
-		if (subject == null) {
-			if (other.subject != null)
-				return false;
-		} else if (!subject.equals(other.subject))
-			return false;
-		if (to == null) {
-			if (other.to != null)
-				return false;
-		} else if (!to.equals(other.to))
-			return false;
-		return true;
-	}
+    @Override
+    public boolean equals(Object other) {
+        if (!(other instanceof EmailMessage)) {
+            return false;
+        }
+        EmailMessage castOther = (EmailMessage) other;
+        return new EqualsBuilder().append(subject, castOther.subject).append(body, castOther.body).append(to, castOther.to).isEquals();
+    }
 
-	@Override
-	public String toString() {
-		return "EmailMessage [subject=" + subject + ", body=" + body + ", to="
-				+ to + "]";
-	}
-	
+    @Override
+    public int hashCode() {
+        return new HashCodeBuilder().append(subject).append(body).append(to).hashCode();
+    }
+
+    @Override
+    public String toString() {
+        return "EmailMessage [subject=" + subject + ", body=" + body + ", to=" + to + "]";
+    }
+
 }

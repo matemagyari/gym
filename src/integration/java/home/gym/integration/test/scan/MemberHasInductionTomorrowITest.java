802ff0403f712193a76a703606e3f2e33f21b9b6
Mate Magyari <Mate.Magyari@Gamesys.co.uk>
2013-03-25 15:11:43 +0000

    no message

--- src/integration/java/home/gym/integration/test/scan/MemberHasInductionTomorrowITest.java
+++ src/integration/java/home/gym/integration/test/scan/MemberHasInductionTomorrowITest.java
@@ -6,7 +6,7 @@ import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
 @Cucumber.Options(features={"src/test/resources/features/member_has_induction_tomorrow.feature"}
-				 ,glue    ={"home.gym.integration.test.steps"})
+				 ,glue    ={"home.gym.integration.test.stepdefinitions"})
 
 public class MemberHasInductionTomorrowITest {
 
